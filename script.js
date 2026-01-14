c7d2849cc70ea72922db4782923d4cf3532e644a
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-14 17:03:13 +0800

    hover and color change

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // select container for all square div 
 const squareDivContainer = document.querySelector(".square-div-container");
 
+// create 16 * 16 grid
 for (let i = 0; i < 16; i++) {
     const columnSquareDiv = document.createElement("div")
     for (let j = 0; j < 16; j++) {
         const squareDiv = document.createElement("div");
-        squareDiv.style.border = "1px solid black";
-        squareDiv.style.padding = "24px";
-        squareDiv.style.margin = "3px";
+        squareDiv.classList.add("pixel-box");
         columnSquareDiv.appendChild(squareDiv);
     }
     squareDivContainer.appendChild(columnSquareDiv);
 }
+
+
+// hover and change color (add a new division and change)
+document.querySelectorAll(".pixel-box").forEach(box => {
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "black";
+    });
+});
